00bb36b492c1affe3554b124a888df2b0b63519a
Jongtaek Choi <lastchoice86@naver.com>
2024-12-12 11:46:13 +0900

    feat: create qrcode script

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import QRCode from "qrcode";
+import fs from "fs";
+
+const generateQRCode = async (
+  text: string,
+  filename: string = "qrcode.png"
+) => {
+  try {
+    const qrCode = await QRCode.toFile(filename, text);
+    return qrCode;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+generateQRCode(process.argv[2], process.argv[3]);
